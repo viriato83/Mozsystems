25e17d387836471c1c1f4bea8340a053bcf032d6
viriato <viriatobonifa22@gmail.com>
2025-04-28 12:35:40 +0200

    first commit2

--- src/Router.js
+++ src/Router.js
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Servicos from "./pages/Servicos";
 import Portfolio from "./pages/Portfolio";
